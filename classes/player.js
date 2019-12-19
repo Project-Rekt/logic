7a1d49b344e9589175936c679e87c9c6ade84e35
KingJeremyNg <jeremy97ng@gmail.com>
2019-12-19 17:17:17 -0500

    Completed radius and targeting function. Added some class variables.

--- classes/player.js
+++ classes/player.js
@@ -1,13 +1,16 @@
+//Class and Constructor to initiate player object.
 class player {
     constructor(currency) {
         this.currency = currency;
     }
 }
 
+//Function to add money.
 player.gainMoney = function(money) {
     this.currency += money;
 }
 
+//Function to subtract money.
 player.spendMoney = function(money) {
     this.currency -= money;
 }
