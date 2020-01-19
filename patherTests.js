83a0df22722be2ebadd4447d19536f1079c14ba0
Derek Lee <55592700+SeaBreamQueen@users.noreply.github.com>
2020-01-18 21:16:08 -0500

    Update patherTests.js

--- patherTests.js
+++ patherTests.js
@@ -11,7 +11,7 @@ class PathTester{
         if (p1.length != p2.length){
             return false
         }
-        for(var i = 0; i < p1.length; i++){
+        for(let i = 0; i < p1.length; i++){
             if (p1[i][0] != p2[i][0] || p1[i][1] != p2[i][1]){
                 return false
             }
@@ -20,14 +20,14 @@ class PathTester{
     }
 
     selfVerifyTest(){
-        var results = []
+        let results = []
         results.push(this.verifyTest1())
         results.push(this.verifyTest2())
         results.push(this.verifyTest3())
         results.push(this.verifyTest4())
         results.push(this.verifyTest5())
-        var failed = false
-        for(var i = 0; i < results.length; i++)
+        let failed = false
+        for(let i = 0; i < results.length; i++)
         {
             if(!results[i]){
                 failed = true
@@ -40,12 +40,12 @@ class PathTester{
     }
 
     pathTest(){
-        var results = []
+        let results = []
         results.push(this.pathTest1())
         results.push(this.pathTest2())
         results.push(this.pathTest3())
         var failed = false
-        for(var i = 0; i < results.length; i++)
+        for(let i = 0; i < results.length; i++)
         {
             if(!results[i]){
                 failed = true
@@ -58,81 +58,81 @@ class PathTester{
     }
 
     verifyTest1(){
-        var a = []
-        var b = []
-        var result = this.pathsEqual(a, b)
+        let a = []
+        let b = []
+        let result = this.pathsEqual(a, b)
         return result
     }
 
     verifyTest2(){
-        var a = [[0, 0]]
-        var b = [[0, 0]]
-        var result = this.pathsEqual(a, b)
+        let a = [[0, 0]]
+        let b = [[0, 0]]
+        let result = this.pathsEqual(a, b)
         return result
     }
     verifyTest3(){
-        var a = [[0, 0]]
-        var b = [[0, 1]]
-        var result = !this.pathsEqual(a, b)
+        let a = [[0, 0]]
+        let b = [[0, 1]]
+        let result = !this.pathsEqual(a, b)
         return result
     }
     verifyTest4(){
-        var a = [[0, 0]]
-        var b = [[0, 0], [0, 0]]
-        var result = !this.pathsEqual(a, b)
+        let a = [[0, 0]]
+        let b = [[0, 0], [0, 0]]
+        let result = !this.pathsEqual(a, b)
         return result
     }
     verifyTest5(){
-        var a = [[0, 1], [2, 3], [4, 5]]
-        var b = [[0, 1], [2, 3], [4, 5]]
-        var result = this.pathsEqual(a, b)
+        let a = [[0, 1], [2, 3], [4, 5]]
+        let b = [[0, 1], [2, 3], [4, 5]]
+        letlet result = this.pathsEqual(a, b)
         return result
     }
 
 
     pathTest1(){
-        var p = new Pather()
-        var matrix = [
+        let p = new Pather()
+        let matrix = [
             [0, 1],
             [0, 2]
         ]
-        var start = [0, 0]
-        var expected = [[0, 0], [1, 0], [1, 1]]
+        let start = [0, 0]
+        let expected = [[0, 0], [1, 0], [1, 1]]
         p.initializeGraph(matrix)
-        var path = p.findShortestPathToEnds(start[0], start[1])
-        var result = this.pathsEqual(path, expected)
+        let path = p.findShortestPathToEnds(start[0], start[1])
+        let result = this.pathsEqual(path, expected)
         return result
     }
     pathTest2(){
-        var p = new Pather()
-        var matrix = [
+        let p = new Pather()
+        let matrix = [
             [0, 1],
             [2, 2]
         ]
-        var start = [0, 0]
-        var expected = [[0, 0],[1, 0]]
+        let start = [0, 0]
+        let expected = [[0, 0],[1, 0]]
         p.initializeGraph(matrix)
-        var path = p.findShortestPathToEnds(start[0], start[1])
-        var result = this.pathsEqual(path, expected)
+        let path = p.findShortestPathToEnds(start[0], start[1])
+        let result = this.pathsEqual(path, expected)
         return result
     }
     pathTest3(){
-        var p = new Pather()
-        var matrix = [
+        let p = new Pather()
+        let matrix = [
             [0, 1, 1, 0],
             [0, 0, 0, 0],
             [1, 1, 1, 0],
             [2, 0, 2, 0]
         ]
-        var start = [0, 0]
-        var expected = [[0, 0], [1, 0], [1, 1], [1, 2], [1, 3], [2, 3], [3, 3], [3, 2]]
+        let start = [0, 0]
+        let expected = [[0, 0], [1, 0], [1, 1], [1, 2], [1, 3], [2, 3], [3, 3], [3, 2]]
         p.initializeGraph(matrix)
-        var path = p.findShortestPathToEnds(start[0], start[1])
-        var result = this.pathsEqual(path, expected)
+        let path = p.findShortestPathToEnds(start[0], start[1])
+        let result = this.pathsEqual(path, expected)
         return result
     }
 }
 
-var tester = new PathTester()
+let tester = new PathTester()
 tester.selfVerifyTest()
 tester.pathTest()
