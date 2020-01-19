e2814e59bbb415a54729133b35c8b5ee740a1fc3
Derek Lee <55592700+SeaBreamQueen@users.noreply.github.com>
2020-01-18 21:09:53 -0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-//Babel setup to use ES6 import and export.
-require('babel-register')({
-    presets: ['env']
-});
-
-module.exports = require('./test.js')
-module.exports = require('./patherTests.js')
-module.exports = require('./mobSpawnerTests.js')
