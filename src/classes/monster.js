78559730db20f5a76db993789a1505d8be4d6b21
OneHandKlap <45212899+OneHandKlap@users.noreply.github.com>
2020-03-20 11:36:52 -0400

    added effect.js for statusEffect class

--- src/classes/monster.js
+++ src/classes/monster.js
@@ -1,85 +1,85 @@
 //Class and Constructor to initiate monster object.
-import moveOverPath from "../functions/movement"
+import moveOverPath from "../functions/movement";
 export default class Monster {
-    constructor(hp, speed, def, distance, x, y) {
-        this.hp = hp;
-        this.speed = speed;
-        this.def = def;
-        this.distance = distance;
-        this.positionX = x;
-        this.positionY = y;
-        this.path = []
-        this.step = 0
-        this.reachedGoal = false
-        this.dead = false
-    }
+  constructor(hp, speed, def, distance, x, y) {
+    this.hp = hp;
+    this.speed = speed;
+    this.def = def;
+    this.distance = distance;
+    this.positionX = x;
+    this.positionY = y;
+    this.path = [];
+    this.step = 0;
+    this.reachedGoal = false;
+    this.dead = false;
+    this.effects = [];
+  }
 
-    move(time){
-        moveOverPath(this, time)
-    }
+  move(time) {
+    moveOverPath(this, time);
+  }
 
-    hasReachedGoal(){
-        this.reachedGoal = this.step + 1 >= this.path.length
-        return this.reachedGoal
-    }
+  hasReachedGoal() {
+    this.reachedGoal = this.step + 1 >= this.path.length;
+    return this.reachedGoal;
+  }
 
-    setPath(path){
-        this.path = path
-        this.step = 0
-    }
+  setPath(path) {
+    this.path = path;
+    this.step = 0;
+  }
 
-    setStep(step){
-        this.step = step
-    }
+  setStep(step) {
+    this.step = step;
+  }
 
-    //Function to subtract health.
-    takeDamage(damage) {
-        this.hp -= damage;
-    }
+  //Function to subtract health.
+  takeDamage(damage) {
+    this.hp -= damage;
+  }
 
-    //Function to add health.
-    healDamage(heal) {
-        this.hp += heal;
-    }
+  //Function to add health.
+  healDamage(heal) {
+    this.hp += heal;
+  }
 
-    //position given in [y, x] format
-    updatePosition(position) {
-        this.positionX = position[1];
-        this.positionY = position[0];
-    }
+  //position given in [y, x] format
+  updatePosition(position) {
+    this.positionX = position[1];
+    this.positionY = position[0];
+  }
 
-    //Function to change distance.
-    setDistance(distance) {
-        this.distance = distance;
-    }
+  //Function to change distance.
+  setDistance(distance) {
+    this.distance = distance;
+  }
 
-    getHp(){
-        return this.hp
-    }
-    getSpeed(){
-        return this.speed
-    }
-    getDef(){
-        return this.def
-    }
-    getDistance(){
-        return this.distance
-    }
-    //position given in [y, x] format
-    getPosition(){
-        return [this.positionY, this.positionX]
-    }
-    getPath(){
-        return this.path
-    }
-    getStep(){
-        return this.step
-    }
-    isDead(){
-        this.dead = this.hp <= 0
-        return this.dead
-    }
-    
+  getHp() {
+    return this.hp;
+  }
+  getSpeed() {
+    return this.speed;
+  }
+  getDef() {
+    return this.def;
+  }
+  getDistance() {
+    return this.distance;
+  }
+  //position given in [y, x] format
+  getPosition() {
+    return [this.positionY, this.positionX];
+  }
+  getPath() {
+    return this.path;
+  }
+  getStep() {
+    return this.step;
+  }
+  isDead() {
+    this.dead = this.hp <= 0;
+    return this.dead;
+  }
 }
 
 //export { Monster };
